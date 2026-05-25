5ab668c1b16d10c0cd8852601d297bf297bb5e53
Ryunaja <Kawiwat.ryu@gmail.com>
2026-05-25 15:19:38 +0700

    add pwa and tailwindcss to package list

--- frontend/owlshelf/vite.config.ts
+++ frontend/owlshelf/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
+  css: {
+    postcss: { plugins: [tailwindcss, autoprefixer] },
+  },
+});
